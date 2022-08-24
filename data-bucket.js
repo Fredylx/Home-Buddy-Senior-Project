eccb633169431924ea53e0fe3210af5aaa772082
Fred Lopez <84998401+Fredylx@users.noreply.github.com>
2022-08-24 11:43:50 -0700

    Create data-bucket.js

---
+++ data-bucket.js
@@ -0,0 +1,50 @@
+
+//Title: Data Bucket (in Javascript)
+
+var port = 81;
+var server;
+var count = 0;
+
+function setup() {
+	// create server
+	server = new TCPServer();
+	
+	// when new client is connected
+	server.onNewClient = function(client) {
+	
+		Serial.println("new client: " + client.remoteIP()
+			+ ":" + client.remotePort());
+			
+		// when the client state changes
+		client.onConnectionChange = function(type) {
+			Serial.println("connection changed: " + type);
+		};
+		
+		// when receiving data from client
+		client.onReceive = function(data) {
+			// print it and then disconnect
+			Serial.println("received: " + client.remoteIP()
+				+ ":" + client.remotePort() + ", " + data);
+			//client.close();
+			saveData(data);
+		};
+		
+		// send a msg to client
+		//client.send("hello " + (count++));
+	};
+	
+	// start listening on port
+	Serial.println(server.listen(port));
+}
+
+function saveData(d) {
+	// write to disk
+	console.log("Saving door status ",d," to disk...");
+	//console.log(FileSystem.exists("/door_status.txt"));
+	var file = FileSystem.open('/door_status.txt',
+	File.WRITE | File.READ);
+	file.println(d);
+	file.close();
+}
+
+
